c80f6a453a009f94193d821301827779fb536a98
samx23 <skalexsong@gmail.com>
2021-05-26 17:33:22 +0700

    Update : proper use of arrow component

--- src/component/home/Hero.js
+++ src/component/home/Hero.js
@@ -1,28 +1,26 @@
 import { Col, Container, Row } from "react-bootstrap";
 import Separator from "../separator";
 
-const Hero = () => {
-  return (
-    <Container className="hero text-center text-white">
-      <Row>
-        <Col sm={12} className=" mt-5 mb-5">
-          <div className="h1">BAKULSEPATU</div>
-          <h1 className="mt-4 hero-title">
-            Cari Sepatu Kasual Keren dan Kekinian?
-          </h1>
-          <p className="mt-4">
-            Bakul sepatu aja, sepatu berkualitas original dengan gaya kekinian
-            dengan harga murah, plus banyak promonya!
-          </p>
-          <a href="#product-first" className="btn mybtn mb-5 mt-3 px-5">
-            Lihat Produk
-          </a>
-          <Separator className="pb-5" />
-          <Separator className="pb-2" />
-        </Col>
-      </Row>
-    </Container>
-  );
-};
+const Hero = () => (
+  <Container className="hero text-center text-white">
+    <Row>
+      <Col sm={12} className=" mt-5 mb-5">
+        <div className="h1">BAKULSEPATU</div>
+        <h1 className="mt-4 hero-title">
+          Cari Sepatu Kasual Keren dan Kekinian?
+        </h1>
+        <p className="mt-4">
+          Bakul sepatu aja, sepatu berkualitas original dengan gaya kekinian
+          dengan harga murah, plus banyak promonya!
+        </p>
+        <a href="#product-first" className="btn mybtn mb-5 mt-3 px-5">
+          Lihat Produk
+        </a>
+        <Separator className="pb-5" />
+        <Separator className="pb-2" />
+      </Col>
+    </Row>
+  </Container>
+);
 
 export default Hero;
